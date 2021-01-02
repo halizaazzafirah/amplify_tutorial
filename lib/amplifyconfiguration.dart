a6c1f8aaf175ebc95f60adc48870491149e642ea
halizaazzafirah <73262350+halizaazzafirah@users.noreply.github.com>
2021-01-02 22:30:54 +0800

    first commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:27980247-5f1f-4d5c-81d9-edf13fd39053",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_WmoXQRFuG",
+                        "AppClientId": "4t4mrt25rs708ef1fg2u3si1cr",
+                        "AppClientSecret": "a6lpit479cpk355eqc1pbcajie8nnt4msaerf2l1dsuv48gbubv",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
